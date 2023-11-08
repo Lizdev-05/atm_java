bb87e925e10545f69c0f7cc862bdc842b71b6d26
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-08 02:02:55 +0000

    prompt user to enter their atm pin with scanner

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int atmNumber = 987654321;
-        int atmPin = 1234;
+        int defaultAtmNumber = 987654321;
+        int defaultAtmPin = 1234;
 
         Scanner logIn = new Scanner(System.in);
         System.out.print("Enter your atm number:  ");
         int atmNumber = logIn.nextInt();
+        System.out.print("Enter your atm pin: ");
+        int atmPin = logIn.nextInt();
     }
 }
