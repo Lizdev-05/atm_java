261f81e36ab7d8df18a229635f18343f977f1706
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-11-08 02:00:54 +0000

    prompt user to enter their atm number with scanner

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static void main(String[] args) {
+        int atmNumber = 987654321;
+        int atmPin = 1234;
+
+        Scanner logIn = new Scanner(System.in);
+        System.out.print("Enter your atm number:  ");
+        int atmNumber = logIn.nextInt();
+    }
 }
